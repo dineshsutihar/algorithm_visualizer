59fe958a530b173f1c3c157ce4427c9fe8f0bca2
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-06-20 03:15:46 +0530

    feat - implements stop functonality which reload the website to stop the execution of code

--- scripts/main.js
+++ scripts/main.js
@@ -1,84 +1,105 @@
-
 //Variables (BE CAREFUL THESE MIGHT BE USED IN OTHER JS FILES TOO)
-var inp_as=document.getElementById('a_size'),array_size=inp_as.value;
-var inp_gen=document.getElementById("a_generate");
-var inp_aspeed=document.getElementById("a_speed");
-//var array_speed=document.getElementById('a_speed').value;
+var inp_as = document.getElementById("a_size"),
+  array_size = inp_as.value;
+var inp_gen = document.getElementById("a_generate");
+var inp_aspeed = document.getElementById("a_speed");
 
-var butts_algos=document.querySelectorAll(".algos button");
+var stop_btn = document.getElementById("a_stop");
 
-var div_sizes=[];
-var divs=[];
+var butts_algos = document.querySelectorAll(".algos button");
+
+var div_sizes = [];
+var divs = [];
 var margin_size;
-var cont=document.getElementById("array_container");
-cont.style="flex-direction:row";
+var cont = document.getElementById("array_container");
+cont.style = "flex-direction:row";
 
 //Array generation and updation.
 
-inp_gen.addEventListener("click",generate_array);
-inp_as.addEventListener("input",update_array_size);
+inp_gen.addEventListener("click", generate_array);
+inp_as.addEventListener("input", update_array_size);
+stop_btn.addEventListener("click",()=> window.location.reload())
 
-function generate_array()
-{
-    cont.innerHTML="";
+function generate_array() {
+  cont.innerHTML = "";
 
-    for(var i=0;i<array_size;i++)
-    {
-        div_sizes[i]=Math.floor(Math.random() * 0.5*(inp_as.max - inp_as.min) ) + 10;
-        divs[i]=document.createElement("div");
-        cont.appendChild(divs[i]);
-        margin_size=0.1;
-        divs[i].style=" margin:0% " + margin_size + "%; background-color:blue; width:" + (100/array_size-(2*margin_size)) + "%; height:" + (div_sizes[i]) + "%;";
-    }
+  for (var i = 0; i < array_size; i++) {
+    div_sizes[i] =
+      Math.floor(Math.random() * 0.5 * (inp_as.max - inp_as.min)) + 10;
+    divs[i] = document.createElement("div");
+    cont.appendChild(divs[i]);
+    margin_size = 0.1;
+    divs[i].style =
+      " margin:0% " +
+      margin_size +
+      "%; background-color:blue; width:" +
+      (100 / array_size - 2 * margin_size) +
+      "%; height:" +
+      div_sizes[i] +
+      "%;";
+  }
 }
 
-function update_array_size()
-{
-    array_size=inp_as.value;
-    generate_array();
+function update_array_size() {
+  array_size = inp_as.value;
+  generate_array();
 }
 
-window.onload=update_array_size();
+window.onload = update_array_size();
 
 //Running the appropriate algorithm.
-for(var i=0;i<butts_algos.length;i++)
-{
-    butts_algos[i].addEventListener("click",run_algo);
+for (var i = 0; i < butts_algos.length; i++) {
+  butts_algos[i].addEventListener("click", run_algo);
 }
 
-function disable_buttons()
-{
-    for(var i=0;i<butts_algos.length;i++)
-    {
-        butts_algos[i].classList=[];
-        butts_algos[i].classList.add("butt_locked");
+function disable_buttons() {
+  for (var i = 0; i < butts_algos.length; i++) {
+    butts_algos[i].classList = [];
+    butts_algos[i].classList.add("butt_locked");
 
-        butts_algos[i].disabled=true;
-        inp_as.disabled=true;
-        inp_gen.disabled=true;
-        inp_aspeed.disabled=true;
-    }
+    butts_algos[i].disabled = true;
+    inp_as.disabled = true;
+    inp_gen.disabled = true;
+    inp_aspeed.disabled = true;
+  }
 }
 
-function run_algo()
-{
-    disable_buttons();
+function run_algo() {
+//   disable_buttons();
 
-    this.classList.add("butt_selected");
-    switch(this.innerHTML)
-    {
-        case "Bubble":Bubble();
-                        break;
-        case "Selection":Selection_sort();
-                        break;
-        case "Insertion":Insertion();
-                        break;
-        case "Merge":Merge();
-                        break;
-        case "Quick":Quick();
-                        break;
-        case "Heap":Heap();
-                        break;
-    }
+  stop_btn.style.opacity = 1;
+
+  this.classList.add("butt_selected");
+  switch (this.innerHTML) {
+    case "Bubble":
+      Bubble();
+      stop_btn.style.display = 0;
+
+      break;
+    case "Selection":
+      Selection_sort();
+      stop_btn.style.display = 0;
+
+      break;
+    case "Insertion":
+      Insertion();
+      stop_btn.style.display = 0;
+
+      break;
+    case "Merge":
+      Merge();
+      stop_btn.style.display = 0;
+
+      break;
+    case "Quick":
+      Quick();
+      stop_btn.style.display = 0;
+
+      break;
+    case "Heap":
+      Heap();
+      stop_btn.style.display = 0;
+
+      break;
+  }
 }
-
