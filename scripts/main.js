2962c0ba053600cd727a46b5a19f9223dace1296
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-06-21 00:51:18 +0530

    refactor - changed all the initilization for legecy to es5 code

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,17 @@
 //Variables (BE CAREFUL THESE MIGHT BE USED IN OTHER JS FILES TOO)
-var inp_as = document.getElementById("a_size"),
+let inp_as = document.getElementById("a_size"),
   array_size = inp_as.value;
-var inp_gen = document.getElementById("a_generate");
-var inp_aspeed = document.getElementById("a_speed");
+const inp_gen = document.getElementById("a_generate");
+const inp_aspeed = document.getElementById("a_speed");
 
-var stop_btn = document.getElementById("a_stop");
+const stop_btn = document.getElementById("a_stop");
 
-var butts_algos = document.querySelectorAll(".algos button");
+const butts_algos = document.querySelectorAll(".algos button");
 
-var div_sizes = [];
-var divs = [];
-var margin_size;
-var cont = document.getElementById("array_container");
+let div_sizes = [];
+let divs = [];
+let margin_size;
+const cont = document.getElementById("array_container");
 cont.style = "flex-direction:row";
 
 //Array generation and updation.
@@ -23,7 +23,7 @@ stop_btn.addEventListener("click",()=> window.location.reload())
 function generate_array() {
   cont.innerHTML = "";
 
-  for (var i = 0; i < array_size; i++) {
+  for (let i = 0; i < array_size; i++) {
     div_sizes[i] =
       Math.floor(Math.random() * 0.5 * (inp_as.max - inp_as.min)) + 10;
     divs[i] = document.createElement("div");
@@ -48,12 +48,12 @@ function update_array_size() {
 window.onload = update_array_size();
 
 //Running the appropriate algorithm.
-for (var i = 0; i < butts_algos.length; i++) {
+for (let i = 0; i < butts_algos.length; i++) {
   butts_algos[i].addEventListener("click", run_algo);
 }
 
 function disable_buttons() {
-  for (var i = 0; i < butts_algos.length; i++) {
+  for (let i = 0; i < butts_algos.length; i++) {
     butts_algos[i].classList = [];
     butts_algos[i].classList.add("butt_locked");
 
@@ -65,7 +65,7 @@ function disable_buttons() {
 }
 
 function run_algo() {
-//   disable_buttons();
+  disable_buttons();
 
   stop_btn.style.opacity = 1;
 
